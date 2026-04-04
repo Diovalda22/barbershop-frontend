3d720bb3e6363050f66e402133e59cd8f8685f16
Diovalda22 <ferrenxx@gmail.com>
2026-04-05 02:08:25 +0700

    fix

--- src/router/publicRoutes.tsx
+++ src/router/publicRoutes.tsx
@@ -5,7 +5,7 @@
 
 import type { RouteObject } from 'react-router-dom'
 import { PublicLayout } from '@/layouts/PublicLayout'
-import { LandingPage } from '@/features/landing/pages/LandingPage'
+import { LandingPage } from '@/features/landing/pages/pointcut/LandingPage'
 
 export const publicRoutes: RouteObject[] = [
   {
